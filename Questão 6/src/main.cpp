292be4fa7fd4e4600c4d4dd53ec6997c728c580c
Carlos Vinicius dos Santos <carlosv2s@A307-10L.imd.local>
2018-10-23 21:57:26 -0300

    Questão 6 terminada

--- Questão 6/src/main.cpp
+++ Questão 6/src/main.cpp
@@ -1,11 +1,24 @@
-#include "sorteadora.h"
-#include "cartela.h"
-
+#include "livro.h"
+#include "biblioteca.h"
+#include <string>
 #include <iostream>
 using namespace std;
-
 int main(){
-	Cartela *c = new Cartela();
-	cout<<c;
+	Livro *l1 = new Livro("Clube Da Luta","Chuck Palahniuk","Seila","LeYa","1996","8580444497");
+	Livro *l2 = new Livro();
+	Biblioteca *b = new Biblioteca();
+	b->addLivro(l1);
+	b->addLivro(l2);
+	b->BuscarNome("Clube Da Luta");
+	b->BuscarNome("Harry Poter");
+	b->BuscarIsbn("8580444497");
+	b->BuscarIsbn("13");
+	
+	cout<<b->VerificarExiste(l1)<<endl;
+	cout<<b->VerificarDisponiveis()<<endl;
+	b->emprestar(0);
+	cout<<b->VerificarDisponiveis()<<endl;
+	b->emprestar(1);
+	cout<<b->VerificarDisponiveis()<<endl;
 	return 0;
-}
+} 
